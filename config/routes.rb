1259715663f4ab4bb191a59e0992be5552998792
EC2 Default User <ec2-user@ip-172-31-27-27.ap-northeast-1.compute.internal>
2024-03-12 05:10:42 +0000

    devise 入力フォーム作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+# 社員
+devise_for :employees,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
+# 管理者
+devise_for :admin,skip: [:passwords], controllers: {
+  registrations: "admin/registrations",
+  sessions: "admin/sessions"
+}
+
 end
