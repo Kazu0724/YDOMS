f492585d788a583ad6a41947d814ad1cee957d31
EC2 Default User <ec2-user@ip-172-31-27-27.ap-northeast-1.compute.internal>
2024-03-14 02:07:04 +0000

    管理者　ジャンル機能作成　登録NG

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -7,10 +7,8 @@ class Admin::GenresController < ApplicationController
   def create
     @genre = Genre.new(genre_params)
     if @genre.save
-      flash[:success] = "ジャンルが作成されました。"
       redirect_to admin_genres_path
     else
-      flash.now[:error] = "ジャンルが作成できませんでした。"
       @genres = Genre.all
       render 'index'
     end
@@ -26,13 +24,17 @@ class Admin::GenresController < ApplicationController
   def update
     @genre = Genre.find(params[:id])
     if @genre.update(genre_params)
-      flash[:success] = "ジャンル名が更新されました。"
       redirect_to admin_genres_path
     else
-      flash.now[:error] = "ジャンル名を入力してください。"
       @genre = Genre.find(params[:id])
       render 'edit'
     end
+  end
+
+  private
+
+  def genre_params
+    params.require(:genre).permit(:name, :color)
   end
 
 end
