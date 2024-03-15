f8e2a41405c500343bad9b678459161d3e89455a
EC2 Default User <ec2-user@ip-172-31-27-27.ap-northeast-1.compute.internal>
2024-03-15 07:17:02 +0000

    管理者　社員一覧途中

--- app/controllers/admin/employees_controller.rb
+++ app/controllers/admin/employees_controller.rb
@@ -1,2 +1,24 @@
 class Admin::EmployeesController < ApplicationController
+  def index
+    @employees = Employee.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def employee_params
+    params.require(:employee).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :employee_number, :phone_number, :email, :car_number)
+  end
+
 end
