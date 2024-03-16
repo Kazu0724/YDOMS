0802761db31f4ebcdc8d6a1b175f05dc3a4a97a7
EC2 Default User <ec2-user@ip-172-31-27-27.ap-northeast-1.compute.internal>
2024-03-16 08:13:49 +0000

    管理者側　配送先一覧・登録画面　作成

--- app/controllers/admin/addresses_controller.rb
+++ app/controllers/admin/addresses_controller.rb
@@ -1,2 +1,42 @@
 class Admin::AddressesController < ApplicationController
+
+  def index
+    @addresses = Address.all
+    @genres = Genre.all
+  end
+
+  def new
+    @address = Address.new
+    @genres = Genre.all
+  end
+
+  def create
+    @address = Address.new(address_params)
+    if @address.save
+      redirect_to admin_addresses_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @genres = Genre.all
+  end
+
+  def edit
+    @genres = Genre.all
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def address_params
+    params.require(:address).permit(:genre_id, :name, :postal_code, :address, :phone_number)
+  end
+
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,3 +1,5 @@
 class Genre < ApplicationRecord
+  has_many :addresses, dependent: :destroy
+
   enum color: { red:0, blue:1, yellow:2, lime:3, purple:4, orenge:5 }
 end
