0c7149f13c6512272f2381e4a6c76ab31a35b429
EC2 Default User <ec2-user@ip-172-31-27-27.ap-northeast-1.compute.internal>
2024-03-23 18:39:03 +0000

    検索フォーム設置　動作NG

--- app/controllers/admin/searches_controller.rb
+++ app/controllers/admin/searches_controller.rb
@@ -1,2 +1,13 @@
 class Admin::SearchesController < ApplicationController
+  def search
+    @model = params[:model]
+    @content = params[:content]
+    if @model == 'work'
+      @records = Work.search_for(@content)
+    elsif @model == 'employee'
+      @records = Employee.search_for(@content)
+    else
+      @records = Address.search_for(@content)
+    end
+  end
 end
--- app/models/work.rb
+++ app/models/work.rb
@@ -5,4 +5,8 @@ class Work < ApplicationRecord
   belongs_to :address
   has_many :work_comments, dependent: :destroy
 
+  def self.search_for(content)
+    Work.where('work.address.name LIKE ?', '%' + content + '%')
+  end
+
 end
