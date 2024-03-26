2f69c54397f978b39c03cfa425ff34cf436126ed
EC2 Default User <ec2-user@ip-172-31-27-27.ap-northeast-1.compute.internal>
2024-03-26 04:31:51 +0000

    ログアウトリンク変更　ヘッダー文字変更

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -14,7 +14,7 @@ class Public::SessionsController < Devise::SessionsController
   end
 
   def after_sign_out_path_for(resource)
-    new_employee_session_path
+    root_path
   end
 
   # GET /resource/sign_in
