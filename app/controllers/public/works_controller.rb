98815620768c4786106d8d2eb7845cfe497859b1
EC2 Default User <ec2-user@ip-172-31-27-27.ap-northeast-1.compute.internal>
2024-04-21 09:46:11 +0000

    配送ステータス　非同期化　修正途中

--- app/controllers/public/works_controller.rb
+++ app/controllers/public/works_controller.rb
@@ -16,10 +16,10 @@ class Public::WorksController < ApplicationController
     @work = Work.find(params[:id])
     if @work.status == true
       @work.update_attribute(:status, false)
-      redirect_to request.referer
+      # redirect_to request.referer
     else
       @work.update_attribute(:status, true)
-      redirect_to request.referer
+      # redirect_to request.referer
     end
   end
 end
