54fe42f643daae3e8c3f25059008f5adad380029
EC2 Default User <ec2-user@ip-172-31-27-27.ap-northeast-1.compute.internal>
2024-04-21 09:27:24 +0000

    コメント機能　非同期化　完了

--- app/controllers/public/work_comments_controller.rb
+++ app/controllers/public/work_comments_controller.rb
@@ -1,17 +1,17 @@
 class Public::WorkCommentsController < ApplicationController
 
   def create
-    work = Work.find(params[:work_id])
-    comment = current_employee.work_comments.new(work_comment_params)
-    comment.work_id = work.id
-    comment.save
-    # redirect_to request.referer
+    @work = Work.find(params[:work_id])
+    @work_comment = WorkComment.new(work_comment_params)
+    @work_comment.work_id = @work.id
+    @work_comment.employee_id = current_employee.id
+    @work_comment.save
   end
 
   def destroy
-    comment = WorkComment.find(params[:id])
-    comment.destroy
-    # redirect_to request.referer
+    @work = Work.find(params[:work_id])
+    @work_comment = WorkComment.find(params[:id])
+    @work_comment.destroy
   end
 
   private
