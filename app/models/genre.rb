3ae7a524887a0d2de71fa0e2b1a36f52f4f63f2a
EC2 Default User <ec2-user@ip-172-31-27-27.ap-northeast-1.compute.internal>
2024-04-24 08:36:22 +0000

    バリデーション　修正途中

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,7 +1,7 @@
 class Genre < ApplicationRecord
   has_many :addresses, dependent: :destroy
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: { scope: :color }
 
   # カラー赤:0, 青:1, 黄:2, 緑:3, 紫:4, 橙:5
   enum color: { salmon:0, deepskyblue:1, yellow:2, greenyellow:3, violet:4, coral:5 }
